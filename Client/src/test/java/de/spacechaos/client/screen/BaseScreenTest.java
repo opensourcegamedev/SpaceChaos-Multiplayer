8274bd841deb20df22dc1f627acf141a6c8c3fcb
crykn <crykn@users.noreply.github.com>
2018-03-26 23:28:15 +0200

    Added a first test for screens.

---
+++ Client/src/test/java/de/spacechaos/client/screen/BaseScreenTest.java
@@ -0,0 +1,51 @@
+package de.spacechaos.client.screen;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import de.spacechaos.client.GameUnitTest;
+
+/**
+ * Tests the {@link BaseScreen} class. Should get extended by the tests for
+ * children of {@link BaseScreen}
+ */
+public class BaseScreenTest extends GameUnitTest {
+
+    protected void testBasicStuff(BaseScreen s) {
+        s.pause();
+        s.resume();
+        
+        s.finishLoading();
+        assertTrue(s.isLoaded());
+    }
+
+    @Test
+    public void test() {
+        BaseScreen s = new BaseScreen() {
+            @Override
+            public void show() {
+            }
+
+            @Override
+            public void render(float delta) {
+            }
+
+            @Override
+            public void hide() {
+            }
+
+            @Override
+            public void dispose() {
+            }
+
+            @Override
+            protected void onInit() {
+            }
+        };
+        
+        testBasicStuff(s);
+        s.resize(15, 15);
+    }
+
+}
