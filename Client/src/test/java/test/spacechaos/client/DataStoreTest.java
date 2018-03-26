30c31d51da2d3808d731a39d32a4a56ec874e8a5
crykn <crykn@users.noreply.github.com>
2018-03-26 16:18:52 +0200

    Added the first tests.

---
+++ Client/src/test/java/test/spacechaos/client/DataStoreTest.java
@@ -0,0 +1,30 @@
+package test.spacechaos.client;
+
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import com.badlogic.gdx.graphics.Color;
+
+import de.spacechaos.client.data.DataStore;
+
+/**
+ * Tests the {@link DataStore} class.
+ */
+public class DataStoreTest {
+
+    @Test
+    public void testHandler() {
+        final String key = "xyz";
+        final Color value = new Color();
+
+        DataStore store = new DataStore();
+        assertTrue(!store.contains(key));
+
+        store.put(key, value);
+        assertTrue(store.contains(key));
+        assertSame(value, store.get(key));
+    }
+
+}
