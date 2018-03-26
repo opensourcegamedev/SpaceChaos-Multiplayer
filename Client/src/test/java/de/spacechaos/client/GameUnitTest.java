14d8bc9cfba74fbd8a7ed4cfe5cbaa20a52600d6
crykn <crykn@users.noreply.github.com>
2018-03-26 19:15:07 +0200

    Formatted the base game unit test class properly.

--- Client/src/test/java/de/spacechaos/client/GameUnitTest.java
+++ Client/src/test/java/de/spacechaos/client/GameUnitTest.java
@@ -14,17 +14,35 @@ public abstract class GameUnitTest {
     // This is our "test" application
     private static Application application;
 
-    // Before running any tests, initialize the application with the headless backend
+    // Before running any tests, initialize the application with the headless
+    // backend
     @BeforeClass
     public static void init() {
         // Note that we don't need to implement any of the listener's methods
         application = new HeadlessApplication(new ApplicationListener() {
-            @Override public void create() {}
-            @Override public void resize(int width, int height) {}
-            @Override public void render() {}
-            @Override public void pause() {}
-            @Override public void resume() {}
-            @Override public void dispose() {}
+            @Override
+            public void create() {
+            }
+
+            @Override
+            public void resize(int width, int height) {
+            }
+
+            @Override
+            public void render() {
+            }
+
+            @Override
+            public void pause() {
+            }
+
+            @Override
+            public void resume() {
+            }
+
+            @Override
+            public void dispose() {
+            }
         });
 
         // Use Mockito to mock the OpenGL methods since we are running headlessly
