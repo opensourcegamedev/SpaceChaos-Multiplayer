f84c3ba7884ce0dcbf61a3fca2de075a4ab6e71c
Constantin Schulte <constantinschulte@web.de>
2018-04-28 17:46:12 +0200

    Update PlayerLobbyDataTest: -> add tests for object created with second constructor (no arguments)

--- Client/src/test/java/de/spacechaos/client/data/PlayerLobbyDataTest.java
+++ Client/src/test/java/de/spacechaos/client/data/PlayerLobbyDataTest.java
@@ -1,9 +1,8 @@
 package de.spacechaos.client.data;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertSame;
-
 import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 /**
  * Tests the {@link PlayerLobbyData} class.
@@ -25,6 +24,18 @@ public class PlayerLobbyDataTest {
         dt.setTeamId(id2);
         assertSame(username2, dt.getUsername());
         assertEquals(id2, dt.getTeamId());
+
+        //test object with second constructor
+        PlayerLobbyData dt2 = new PlayerLobbyData();
+        assertNull(dt2.getUsername());
+        assertEquals(0, dt2.getTeamId());
+
+        dt2.setUsername(username2);
+        dt2.setTeamId(id2);
+        assertSame(username2, dt2.getUsername());
+        assertEquals(id2, dt2.getTeamId());
+
+
     }
 
 }
