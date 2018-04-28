739f487079cf36323e50181dfac4ec437fde922f
Constantin Schulte <constantinschulte@web.de>
2018-04-28 17:37:51 +0200

    Update RandomUtilsTest: -> split tests into two methods -> tested the dice with i != 1 -> try to test random with ~50 % tolerance

--- Client/src/test/java/de/spacechaos/client/util/RandomUtilsTest.java
+++ Client/src/test/java/de/spacechaos/client/util/RandomUtilsTest.java
@@ -1,14 +1,14 @@
 package de.spacechaos.client.util;
 
-import de.spacechaos.client.GameUnitTest;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
-public class RandomUtilsTest extends GameUnitTest {
+public class RandomUtilsTest {
 
     @Test
-    public void test(){
+    public void testRandomNumberCreator(){
         assertEquals(1, RandomUtils.getRandomNumber(1, 1));
 
         int max = 100;
@@ -17,8 +17,23 @@ public class RandomUtilsTest extends GameUnitTest {
             assertTrue(RandomUtils.getRandomNumber(min, max) < max + 1);
             assertTrue(RandomUtils.getRandomNumber(min, max) > min - 1);
         }
-
-        assertTrue(RandomUtils.rollTheDice(1));
     }
 
+    @Test
+    public void testDiceRoller(){
+        assertTrue(RandomUtils.rollTheDice(1));
+
+        int chance = 100;
+        int trues = 0;
+        for(int i = 0; i < chance * 100; ++i){
+            if(RandomUtils.rollTheDice(chance)){
+                ++trues;
+            }
+        }
+        assertTrue(trues > 0);
+        assertTrue(trues < chance * 100);
+
+        assertTrue(trues < 150);
+        assertTrue(trues > 50);
+    }
 }
